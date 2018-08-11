7d1e48f161099dac20ec2ef09c2fedf0d739f71e
ncortes12 <ncortes1415@gmail.com>
2018-08-11 13:40:31 -0700

    setting up on click event to produce a random complement

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,15 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/api/complements/", function(req, res) {
+    console.log("look this route is working! You're the best!");
+    console.log(req.body.id);
+    db.complement
+      .findAll({
+        where: { id: req.body.id }
+      })
+      .then(function(complementBody) {
+        console.log(complementBody);
+      });
   });
 };
