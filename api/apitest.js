1c6ade15e68cecb3fe6224420625d2721ca78b8f
drwphillips <drew@superheroprinting.com>
2018-08-16 18:44:11 -0700

    working api

--- api/apitest.js
+++ api/apitest.js
@@ -1,7 +1,8 @@
-var getInsult = require ("insultapi.js");
+var getInsult = require("../api/insultapi.js");
+var keys = require("../keys.js");
 
-getInsult(function(data){
+getInsult(function (data) {
 
     console.log("this is our data on our test page: " + data);
-    
-    })
+
+})
--- api/insultapi.js
+++ api/insultapi.js
@@ -1,19 +1,16 @@
 var unirest = require("unirest")
-var getInsult = function(cb){
-// These code snippets use an open-source library. http://unirest.io/nodejs
-unirest.get("https://lakerolmaker-insult-generator-v1.p.mashape.com/?mode=random")
-.header("X-Mashape-Key", "NVvEfWYNPymshK5c9sCsJ5Rb7W61p1cIWpGjsnVulTyKqFzI5u")
-//.header("Accept", "text/plain")
-.end(function (response) {
-  //console.log(response.status, response.headers, response.body);
-  console.log(response);
+module.exports = function (cb) {
+  // These code snippets use an open-source library. http://unirest.io/nodejs
+  unirest.get("https://lakerolmaker-insult-generator-v1.p.mashape.com/?mode=random")
+    .header("X-Mashape-Key", "NVvEfWYNPymshK5c9sCsJ5Rb7W61p1cIWpGjsnVulTyKqFzI5u")
+    //.header("Accept", "text/plain")
+    .end(function (response) {
+      //console.log(response.status, response.headers, response.body);
+      
 
-var x = response.body;
-console.log(x)
-cb(x)    
+      var x = response.body;
+      console.log(x)
+      cb(x)
 
-});
-  
-module.exports = getInsult;
-
+    });
 }
